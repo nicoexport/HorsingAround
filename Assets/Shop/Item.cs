b911d2d59205e00fe79ada2fcc4121034a3b56b2
Ruben <56798357+DerBrezel@users.noreply.github.com>
2021-11-06 15:53:00 +0100

    ui generation
    ui generation image

--- Assets/Shop/Item.cs
+++ Assets/Shop/Item.cs
@@ -6,26 +6,30 @@ public class Item
 {
     public int id;
     public string name;
+
+    public int cost;
     public string description;
     public Sprite icon;
     public Dictionary<string, int> stats = new Dictionary<string, int>();
 
-    public Item(int id, string name, 
+    public Item(int id, string name, int cost,
     string description, Dictionary<string, int> stats)
     {
         this.id = id;
+        this.cost = cost;
         this.name = name;
         this.description = description;
         this.icon = Resources.Load<Sprite>("Shop/Art" + name);
         this.stats = stats;
     }
 
-    public Item(Item item)    
+    public Item(Item item)
     {
         this.id = item.id;
+        this.cost = item.cost;
         this.name = item.name;
         this.description = item.description;
-        this.icon = Resources.Load<Sprite>("Shop/Art" + name);
+        this.icon = Resources.Load<Sprite>("Shop/Art/" + name);
         this.stats = item.stats;
     }
 }
--- Assets/Shop/ItemDatabase.cs
+++ Assets/Shop/ItemDatabase.cs
@@ -2,32 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDatabase : MonoBehaviour
+public class ItemDatabase
 {
     public List<Item> items = new List<Item>();
 
-    private void Awake(){
+    public ItemDatabase()
+    {
         BuildDatabase();
     }
 
-    public Item GetItem(int id){
+    public Item GetItem(int id)
+    {
         return items.Find(item => item.id == id);
     }
 
-    public Item GetItem(string itemName){
+    public Item GetItem(string itemName)
+    {
         return items.Find(item => item.name == itemName);
     }
 
-    void BuildDatabase(){
+    void BuildDatabase()
+    {
         items = new List<Item>(){
-            new Item(0, "Frog", "Its a test frog",
+            new Item(0, "Frog",20, "Its a test frog",
             new Dictionary<string, int>
             {
                 {"Cost", 10},
                 {"SpeedMultiplier", 2},
             }),
 
-            new Item(1, "Heart", "Its a test frog",
+            new Item(1, "Heart",300, "Its a test frog",
             new Dictionary<string, int>
             {
                 {"Cost", 20},
