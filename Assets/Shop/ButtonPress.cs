ceead5be502d4d4a34cdd207e9be1353ea4823a3
Ruben <56798357+DerBrezel@users.noreply.github.com>
2021-11-07 14:22:42 +0100

    button delete
    button works and gets deleted onclick

--- Assets/Shop/ButtonPress.cs
+++ Assets/Shop/ButtonPress.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ButtonPress : MonoBehaviour
 {
     public Button button;
     public ItemSO item;
     public PlayerData playerData;
+
+    public UnityEvent sexEffex;
+
 
     void Start()
     {
@@ -17,5 +21,10 @@ public class ButtonPress : MonoBehaviour
     {
         //add current Feature to Player
         playerData.AddItem(item.id);
+        sexEffex.Invoke();
+        Object.Destroy(button.gameObject);
+
+
+
     }
 }
